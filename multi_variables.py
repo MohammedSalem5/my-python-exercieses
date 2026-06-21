af1d179a20968c859ca5ab57904789be23ec4b73
MohammedSalem5 <m57syami@gmail.com>
2026-06-22 00:31:36 +0300

    third code

---
+++ multi_variables.py
@@ -0,0 +1,33 @@
+#x , y , z = "orange" , "Banana" , "Apple"
+#print(x)
+#print(y)
+#print(z)
+
+
+#a=s=d="Cherry"
+#print(a)
+#print(s)
+#print(d)
+
+#fruits=["apple" , "banana" , "grape"]
+#x,y,z= fruits
+#print(x)
+#print(y)
+#print(z)
+
+#p="Python is awesome"
+#print(p)
+
+#x="Python"
+#y="Is"
+#z="Awesome!!"
+#print(x,y,z)
+
+#x="Python "
+#y="Is "
+#z="Awesome!!"
+#print(x + y + z)
+
+X=5
+Y="Mohammed"
+print(X,Y)
