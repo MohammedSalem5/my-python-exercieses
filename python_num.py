31e73f34802376e6ca8de484434dee7b9123b6e2
MohammedSalem5 <m57syami@gmail.com>
2026-06-23 03:06:23 +0300

    fifth code

---
+++ python_num.py
@@ -0,0 +1,24 @@
+x=1
+y=2.8
+z=1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+r = 1
+s = 35656222554887711
+t = -3255522
+
+print(type(r))
+print(type(s))
+print(type(t))
+
+##Float can also be scientific numbers with an "e" to indicate the power of 10.
+
+g = 35e3
+h = 12E4
+j = -87.7e100
+
+print(type(g))
+print(type(h))
+print(type(j))
